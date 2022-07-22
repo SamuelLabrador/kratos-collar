5826c906fd4b0418b690632bcb4e97d3829584fb
SamuelLabrador <samuel.labrador.cs@gmail.com>
2022-07-21 19:43:33 -0700

    Updating collar code to use IMU

--- kratos-collar.ino
+++ kratos-collar.ino
@@ -1,43 +1,52 @@
-#include "LSM6DSOX_CUSTOM.h"
 #include <ArduinoBLE.h>
-//#include <Adafruit_DPS310.h>
 #include <Wire.h>
 #include "icp101xx.h"
 #include <Scheduler.h>
-#include <Adafruit_LC709203F.h>
 #include <queue>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
 
-Adafruit_LC709203F lc;
-typedef struct Packet {
-  uint16_t value[6];
-  float barometerData[2];
-}Packet;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+ICP101xx icp;
+
+typedef struct XLPacket {
+  float data[3];
+} XLPacket;
+
+typedef struct GravityPacket {
+  float data[3];
+} GravityPacket;
+
+
+typedef struct Quaternion {
+  float data[4];
+} Quaternion;
+
+typedef struct PressureTempPacket {
+  float data[2];
+} PressureTempPacket;
+
 BLEService BarbellService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
-BLETypedCharacteristic<Packet> DataCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLENotify | BLEWrite);
-//BLETypedCharacteristic<byte> CommandCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1216", BLEWrite);
 BLETypedCharacteristic<byte> EventCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead | BLENotify);
 BLETypedCharacteristic<int> VoltageCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1218", BLERead | BLENotify);
+BLETypedCharacteristic<XLPacket> LinearAccelerationCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLENotify);
+BLETypedCharacteristic<Quaternion> QuaternionCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1219", BLERead | BLENotify);
+BLETypedCharacteristic<PressureTempPacket> PressureTempCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1220", BLERead | BLENotify);
+BLETypedCharacteristic<GravityPacket> GravityVectorCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1221", BLERead | BLENotify);
 
-
-double GRAVITY = 9.80665;
 const int MA_AMOUNT = 5;
-
-enum COMMAND {
-  SLEEP = 0x00,
-  READY = 0x01
-};
 
 enum XL_TRANSMIT_STATE {
   WAIT_TRANSMIT,
   TRANSMIT
 };
 
+bool closedState = false;
 XL_TRANSMIT_STATE transmitState = WAIT_TRANSMIT;
-ICP101xx icp;
 uint8_t command;
 
-double shiftAndPop(double *window, uint8_t window_size, double element) {
-  double average = 0;
+double shiftAndPop(float *window, uint8_t window_size, double element) {
+  float average = 0;
   for (uint8_t i = 0; i < window_size - 1; i++) {
     window[i] = window[i + 1];
     average += window[i];
@@ -57,11 +66,20 @@ void configureBLE() {
   // set advertised local name and service UUID:
   BLE.setLocalName("Kratos Collar");
   BLE.setAdvertisedService(BarbellService);
-  BarbellService.addCharacteristic(DataCharacteristic);
-//  BarbellService.addCharacteristic(CommandCharacteristic);
+
+  BarbellService.addCharacteristic(LinearAccelerationCharacteristic);
+  BarbellService.addCharacteristic(QuaternionCharacteristic);
+  BarbellService.addCharacteristic(PressureTempCharacteristic);
   BarbellService.addCharacteristic(EventCharacteristic);
-  EventCharacteristic.setValue(0x00);
   BarbellService.addCharacteristic(VoltageCharacteristic);
+
+  // Set default values for characteristics
+  LinearAccelerationCharacteristic.writeValue({0.0, 0.0, 0.0});
+  QuaternionCharacteristic.writeValue({0.0, 0.0, 0.0, 0.0});
+  PressureTempCharacteristic.writeValue({0.0, 0.0});
+  GravityVectorCharacteristic.writeValue({0.0, 0.0, 0.0});
+  VoltageCharacteristic.writeValue(0.0);
+  EventCharacteristic.writeValue(0x00);
 
   // add service
   BLE.addService(BarbellService);
@@ -82,22 +100,14 @@ void configureBarometer() {
 void configureBatteryMeter() {
   Serial.println("Configuring battery meter");
 
-  if (!lc.begin()) {
-    Serial.println("Couldnt find Adafruit LC709203F?\nMake sure a battery is plugged in!");
-    while (1) yield();
-  }
-
-  lc.setThermistorB(3950);
-  lc.setPackSize(LC709203F_APA_1000MAH);
-  lc.setAlarmVoltage(3.8);
-
   Serial.println("Battery meter initialized!");
 }
 
 void configureAccel() {
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
+  if (!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
   }
   Serial.println("IMU initialized");
 }
@@ -112,91 +122,114 @@ void mainLoop() {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
 
-    uint16_t raw_xl_x, raw_xl_y, raw_xl_z;
-    uint16_t gyro_x, gyro_y, gyro_z;
+    sensors_event_t linearXL, gravity;
     double previousAcceleration = -1.0;
 
     uint8_t previousCommand = 0x00;
-    bool icpMeasuring = false;
 
     uint8_t loadCount = 0;
-    double movingAverageXL[MA_AMOUNT] = {};
-    double previousAverage = -1.0;
-    const uint8_t STOP_DURATION = 100;
+    float movingAverageXL[MA_AMOUNT] = {};
+    float previousAverage = -1.0;
+    const uint8_t STOP_DURATION = 600;
+    const uint8_t START_DURATION = 15;
+    bool icpMeasuring = false;
     uint8_t stopCount = STOP_DURATION;
-    const uint8_t START_DURATION = 10;
     uint8_t startCount = START_DURATION;
-    transmitState = WAIT_TRANSMIT; // Should be WAIT_TRANSMIT
+
+    transmitState = WAIT_TRANSMIT;
 
     while (central.connected()) {
+      closedState = digitalRead(3);
+
+      bno.getEvent(&linearXL, Adafruit_BNO055::VECTOR_LINEARACCEL);
+      bno.getEvent(&gravity, Adafruit_BNO055::VECTOR_GRAVITY);
+      imu::Quaternion quaternion = bno.getQuat();
+
+      float quat_w = quaternion.w();
+      float quat_x = quaternion.x();
+      float quat_y = quaternion.y();
+      float quat_z = quaternion.z();
+
+      // Units m/s^2
+      float xl_x = linearXL.acceleration.x;
+      float xl_y = linearXL.acceleration.y;
+      float xl_z = linearXL.acceleration.z;
+
+      float magnitude = sqrt(xl_x * xl_x + xl_y * xl_y + xl_z * xl_z);
+
+      float average = shiftAndPop(movingAverageXL, MA_AMOUNT, magnitude);
+
+      Serial.print("Transmit State: ");
+      Serial.print(transmitState);
+      Serial.print(" The difference: ");
+      Serial.print(abs(average - previousAverage));
+      Serial.print(" The stop count: ");
+      Serial.println(stopCount);
+
+      if (loadCount < MA_AMOUNT) {
+        movingAverageXL[loadCount] = average;
+        loadCount += 1;
+      } else {
+        switch (transmitState) {
+          case WAIT_TRANSMIT:
+            if (previousAverage >= 0 && abs(average - previousAverage) > 0.1) {
+              startCount -= 1;
+            } else {
+              startCount = START_DURATION;
+            }
+
+            if (startCount <= 0) {
+              transmitState = TRANSMIT;
+              EventCharacteristic.writeValue(0x01);
+            }
+            break;
+          case TRANSMIT:
+            if (abs(average - previousAverage) < 0.1) {
+              stopCount -= 1;
+            } else {
+              stopCount = STOP_DURATION;
+            }
+
+            if (stopCount <= 0 || !closedState) {
+              transmitState = WAIT_TRANSMIT;
+              stopCount = 0;
+              EventCharacteristic.writeValue(0x00);
+            }
+            break;
+        }
+        previousAverage = average;
+      }
+
       if (!icpMeasuring) {
         icp.measureStart(ICP101xx::ACCURATE);
         icpMeasuring = true;
       }
 
-      if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && icp.dataReady()) {
-        float pressure = icp.getPressurePa() / 100.0;
+      if (icp.dataReady()) {
+        float pressure = icp.getPressurePa();
         float temperature = icp.getTemperatureC();
-        IMU.readAcceleration(raw_xl_x, raw_xl_y, raw_xl_z);
-        IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
-
-        double xl_x = (raw_xl_x * (4.0 / 32768.0)) * GRAVITY;
-        double xl_y = (raw_xl_y * (4.0 / 32768.0)) * GRAVITY;
-        double xl_z = (raw_xl_z * (4.0 / 32768.0)) * GRAVITY;
-
-        double magnitude = sqrt(xl_x * xl_x + xl_y * xl_y + xl_z * xl_z);
-
-        double average = shiftAndPop(movingAverageXL, MA_AMOUNT, magnitude);
-
-        if (loadCount < MA_AMOUNT) {
-          movingAverageXL[loadCount] = average;
-          loadCount += 1;
-        } else {
-          switch (transmitState) {
-            case WAIT_TRANSMIT:
-              if (previousAverage >= 0 && abs(average - previousAverage) > 0.1) {
-                startCount -= 1;
-              } else {
-                startCount = START_DURATION;
-              }
-
-              if (startCount <= 0) {
-                transmitState = TRANSMIT;
-                EventCharacteristic.setValue(0x01);
-              }
-              break;
-            case TRANSMIT:
-              if (abs(average - previousAverage) < 0.01) {
-                stopCount -= 1;
-              } else {
-                stopCount = STOP_DURATION;
-              }
-
-              if (stopCount <= 0) {
-                transmitState = WAIT_TRANSMIT;
-                EventCharacteristic.setValue(0x00);
-              }
-              break;
-          }
-        }
-
-        previousAverage = average;
 
         if (transmitState == TRANSMIT) {
-          uint64_t zero = 0;
-
-          Packet packet = {raw_xl_x, raw_xl_y, raw_xl_z,
-                           gyro_x, gyro_y, gyro_z,
-                           pressure, temperature};
-
-          DataCharacteristic.writeValue(packet);
+          PressureTempPacket ptPacket = {pressure, temperature};
+          PressureTempCharacteristic.writeValue(ptPacket);
         }
-
         icpMeasuring = false;
       }
 
+
+      if (transmitState == TRANSMIT) {
+        uint64_t zero = 0;
+
+        XLPacket packet = {xl_x, xl_y, xl_z};
+        LinearAccelerationCharacteristic.writeValue(packet);
+
+        Quaternion quatPacket = {quat_w, quat_x, quat_y, quat_z};
+        QuaternionCharacteristic.writeValue(quatPacket);
+      }
+
       previousCommand = command;
-      yield();
+      // Wait 10 milliseconds before getting the reading again
+      delay(20);
     }
 
     digitalWrite(LED_BUILTIN, LOW);
@@ -205,18 +238,30 @@ void mainLoop() {
   }
 }
 
+void clampCheckLoop() {
+  closedState = digitalRead(3);
+  delay(500);
+}
+
 void ledLoop() {
   const unsigned long amt = 250;
   if (BLE.connected()) {
-    switch (transmitState) {
-      case WAIT_TRANSMIT:
-        digitalWrite(LED_BUILTIN, HIGH);
-        break;
-      case TRANSMIT:
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(amt);
-        digitalWrite(LED_BUILTIN, HIGH);
-        break;
+    if (closedState) {
+      switch (transmitState) {
+        case WAIT_TRANSMIT:
+          digitalWrite(LED_BUILTIN, HIGH);
+          break;
+        case TRANSMIT:
+          digitalWrite(LED_BUILTIN, LOW);
+          delay(amt);
+          digitalWrite(LED_BUILTIN, HIGH);
+          break;
+      }
+    } else {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(50);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(amt * 2);
     }
   } else {
     digitalWrite(LED_BUILTIN, LOW);
@@ -227,28 +272,29 @@ void ledLoop() {
 void batteryLoop() {
   if (BLE.connected()) {
     int value = analogRead(A2);
-    VoltageCharacteristic.setValue(value);
+    VoltageCharacteristic.writeValue(value);
   }
 
   delay(5000);  // dont query too often!
 }
 
-void activityDetectionLoop() {
-
-}
-
 void setup() {
   Serial.begin(9600);
 //  while (!Serial) {}
+  Serial.println("Collar warming up...");
   pinMode(LED_BUILTIN, OUTPUT);
 
   configureBLE();
   configureAccel();
   configureBarometer();
+  pinMode(3, INPUT);
 
+  Scheduler.startLoop(clampCheckLoop);
   Scheduler.startLoop(ledLoop);
-  Scheduler.startLoop(mainLoop);
   Scheduler.startLoop(batteryLoop);
+  Scheduler.startLoop(mainLoop);
+  Serial.println("Finished collar startup!");
+  delay(1000);
 }
 
 void loop() {
